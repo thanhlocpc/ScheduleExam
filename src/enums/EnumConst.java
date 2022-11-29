cd64426d3e6eabc7b5d3d7a99789a2a90a78a68b
thanhlocpc <nguyenthanhlocpc@gmail.com>
2022-11-29 09:49:36 +0700

    UP

--- src/enums/EnumConst.java
+++ src/enums/EnumConst.java
@@ -7,12 +7,27 @@ package enums;
 public class EnumConst {
 
     public enum ClassRoomTypeEnum {
-        LAB("Phòng thực hành"),
-        THEORY("Phòng lý thuyết");
+        LAB_ROOM("Phòng thực hành"),
+        THEORY_ROOM("Phòng lý thuyết");
 
         String description;
 
         ClassRoomTypeEnum(String description) {
+            this.description = description;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+    }
+
+    public enum ExamFormsTypeEnum {
+        LAB_EXAM("Thi thực hành"),
+        THEORY_EXAM("Thi lý thuyết");
+
+        String description;
+
+        ExamFormsTypeEnum(String description) {
             this.description = description;
         }
 
