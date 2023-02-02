07bf8cbe94177404fcb72b72eb0e17fd6edb59cf
lele9h0st <hoangtranhuy2001@gmail.com>
2023-02-02 21:27:48 +0700

    change something

--- src/ga/GA.java
+++ src/ga/GA.java
@@ -1,92 +1,97 @@
-package ga;
+package GA;
 
 import SA.SA;
-import models.DateSchedule;
 import models.Schedule;
+import models.Subject;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
-/**
- * @author ThanhLoc
- * @created 2/1/2023
- */
 public class GA {
+    public static final int POP_SIZE = 100;
+    public static final int N_ITER = 500;
     public List<String> dates;
-    private List<Schedule> population;
-    public static final int POP_SIZE = 200;// Population size
-    public static final double MUTATION_RATE = 0.03;
-    public static final int MAX_ITERATIONS = 4000;
+    List<Schedule> schedules =new ArrayList<>();
     Random rd = new Random();
-
-    public GA(List<String> dates) throws IOException {
+    public GA(List<String> dates) {
         this.dates = dates;
-        population = new ArrayList<>(POP_SIZE);
-        for (int i = 0; i < POP_SIZE; i++) {
-            population.add(createRandomSchedule());
-        }
     }
 
-    public Schedule createRandomSchedule() throws IOException {
-        Schedule result = null;
-        while (true) {
-            result = new Schedule(dates);
-            if (result.isAccepted())
-                break;
+    public List<Schedule> createPopulation() throws IOException {
+         Schedule temp;
+        for (int i = 0; i < POP_SIZE; i++) {
+            while (true) {
+                temp = new Schedule(dates);
+                if (temp.isAccepted()){
+                    schedules.add(temp);
+                    break;
+                }
+            }
+        }
+        return schedules;
+    }
+    public Schedule ga() throws IOException, CloneNotSupportedException {
+         createPopulation();
+        int iter = 0;
+        while (iter < N_ITER) {
+            List<Schedule> newSchedule=new ArrayList<>();
+            for(int i=0;i<POP_SIZE;i++){
+                Schedule x=getParentByRandomSelection();
+                Schedule y=getParentByRandomSelection();
+                Schedule child=preproduce3(x,y);
+//                if (rd.nextInt(100) / 100 <= 0.03)
+//                    mutate(child);
+
+                if (child.fitness <4500) {
+                    System.out.println("iter: " + iter);
+                    return child;
+                }
+                newSchedule.add(child);
+            }
+            schedules = newSchedule;
+            iter++;
+        }
+        Collections.sort(schedules, new Comparator<Schedule>() {
+            @Override
+            public int compare(Schedule o1, Schedule o2) {
+                return Double.compare(o1.fitness,o2.fitness);
+            }
+        });
+        return schedules.get(0);
+    }
+
+    public Schedule getParentByRandomSelection() {
+        // Enter your code here
+        return schedules.get(rd.nextInt(POP_SIZE));
+    }
+    public Schedule preproduce3(Schedule x, Schedule y) throws CloneNotSupportedException, IOException {
+        Schedule result=x.clone();
+        Schedule changeSchedule=x.clone();
+        List<Map.Entry<Subject, Set<String>>> sequence = new ArrayList<>();
+        List<Map.Entry<Subject, Set<String>>> subjectMap1=new ArrayList<>(x.getSubjectMap().entrySet());
+        List<Map.Entry<Subject, Set<String>>> subjectMap2=new ArrayList<>(y.getSubjectMap().entrySet());
+        for(int i=0;i<subjectMap1.size();i++){
+            if(rd.nextInt(10) < 5){
+                sequence.add(subjectMap2.get(i));
+            }
+        }
+        for (Map.Entry<Subject, Set<String>> entry : sequence) {
+            changeSchedule.changeSchedule(entry);
+            changeSchedule.fitness();
+            if (changeSchedule.isAccepted()) {
+
+                if ((changeSchedule.fitness < result.fitness)) {
+                    result = (Schedule) changeSchedule.clone();
+                    result.fitness();
+
+                }
+            }
         }
         return result;
     }
 
-    public Schedule execute() throws CloneNotSupportedException{
+    public void mutate(Schedule node) {
 
-        List<Schedule> newPopulation = null;
-        int count = 0;
-        while(count < MAX_ITERATIONS) {
-            newPopulation = new ArrayList<Schedule>();
-            for (int i = 0; i < POP_SIZE; i++) {
-                Schedule x = getParentByRandomSelection();
-                Schedule y = getParentByRandomSelection();
-
-                Schedule child = preproduce(x, y);
-                if(Math.random() > MUTATION_RATE) mutate(child);
-                child.fitness();
-                if(child.fitness < 5000) {
-                    return child;
-                }
-
-                newPopulation.add(child);
-            }
-            population = newPopulation;
-            count++;
-        }
-        Collections.sort(newPopulation);
-        return newPopulation.get(0);
-    }
-
-    // gây đột biến
-    private void mutate(Schedule child) {
-        int count = rd.nextInt(5);
-        for (int i = 0; i < count; i++) {
-            int a = rd.nextInt(dates.size());
-            int b = rd.nextInt(dates.size());
-            DateSchedule dateSchedule1 = child.getDateScheduleByDate(dates.get(a));
-            DateSchedule dateSchedule2 = child.getDateScheduleByDate(dates.get(b));
-            dateSchedule1.setDate(dates.get(a));
-            dateSchedule2.setDate(dates.get(b));
-        }
-    }
-
-    // lai 2 cái thế
-    public Schedule preproduce(Schedule x, Schedule y) {
-
-        return null;
-    }
-
-    public Schedule getParentByRandomSelection() {
-        return population.get(rd.nextInt(POP_SIZE));
     }
 
     public static void main(String[] args) throws IOException, CloneNotSupportedException {
@@ -106,9 +111,10 @@ public class GA {
             System.out.println("==========begin "+ i+" ==============");
             beginTime = System.currentTimeMillis();
             System.out.println("schedule " + i + ":");
-            GA sa = new GA(dates);
-            Schedule result = sa.execute();
+            GA ga = new GA(dates);
+            Schedule result = ga.ga();
             System.out.println(result.fitness);
+            System.out.println("is accepted:"+result.isAccepted());
             result.getDateScheduleList().forEach(item -> {
                 System.out.println(item);
             });
